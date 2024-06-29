5b6e43f3a271f3f766e849f1a3816c80c186d50e
Javor Emir <emir.j2002@gmail.com>
2024-06-29 21:59:04 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google';
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+export const metadata: Metadata = {
+  title: "Portfolio de JAVOR Emir | Développeur web",
+  description: "Portfolio de JAVOR Emir | Développeur web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body className={`${poppins.variable} w-full h-full bg-black text-white`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+  <div className="text-center text-white">
+    <p className="text-8xl font-bol">EMIR</p>
+    <p className="text-8xl font-bol">JAVOR</p>
+  </div>
+</main>
+
+  );
+}
