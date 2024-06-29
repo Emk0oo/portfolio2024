c69bda9fba3707900866ea6f1240d6223ad7bc49
Javor Emir <emir.j2002@gmail.com>
2024-06-30 00:48:24 +0200

    add placement not finished

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -8,13 +8,15 @@ export default function PortfolioContent() {
 
       <div id="content" className="w-full h-full flex flex-col mt-[20%] px-[4%]">
         <div id="hero" className="w-full h-full flex">
-          <div id="hero-content" className="w-full h-full flex justify-center">
-            <div id="leftText">
-                <p className="[writing-mode:vertical-lr] [transform: rotate(180deg)]">Junior Dev</p>
+          <div id="hero-content" className="w-full h-full flex mt-[10%] justify-between">
+            <div id="leftText" className="flex  rotate-180 text-end ">
+                <p className="text-8xl [writing-mode:vertical-lr]">Junior</p>
+                <p className="text-8xl [writing-mode:vertical-lr]">Developer</p>
+
             </div>
-            <div id="rightText" className="flex flex-col">
-                <p className="text-2xl font-thin text-slate-300">Full-stack</p>
-                <p className="text-5xl font-thin">Developer</p>
+            <div id="rightText" className="flex flex-col w-1/2">
+                <p className="text-5xl font-thin text-slate-300">Full-stack</p>
+                <p className="text-8xl font-thin">Developer</p>
                 <p className="text-3xl font-thin max-w-[50%]">Hello! I&apos;m Emir, I&apos;m a junior full-stack developer based in France working for Renault Group</p>
                 <Link href="/" className="text-3xl font-thin max-w-[50%] bg-white text-black p-4 mt-5 w-fit">View Portfolio</Link>
             </div>
