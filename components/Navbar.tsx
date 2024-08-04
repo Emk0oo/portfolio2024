ab0bef95c275ae5af54e42828983180d5ee585a6
Javor Emir <emir.j2002@gmail.com>
2024-08-04 19:33:59 +0200

    navbar button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="text-white mx-[10%] mt-2 shadow">
+    <header className=" mx-[10%] mt-2 shadow">
       <div className="container  py-3">
         <div className="flex items-center justify-between">
             <div>
@@ -14,19 +14,22 @@ const Navbar = () => {
                 </a>
             </div>
     
-            <nav className="space-x-4 text-lg ">
-                <a className="hover:text-gray-300" href="#about">
+            <nav className="space-x-2 text-lg ">
+                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#about">
                 About
                 </a>
-                <a className="hover:text-gray-300" href="#portfolio">
+                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#portfolio">
                 Timeline
                 </a>
-                <a className="hover:text-gray-300" href="#services">
+                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#services">
                 Technology
                 </a>
-                <a className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full " href="#contact">
+                <a className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full" href="#contact">
                 Contact me
                 </a>
+                {/* <button id="changeLanguage" className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full">
+                Change Language
+                </button> */}
             </nav>
         </div>
       </div>
