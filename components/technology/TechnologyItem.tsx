d6e4efaa2b22a537be4ad0e6d7a0cfffcb4cc14f
Javor Emir <emir.j2002@gmail.com>
2024-08-05 00:25:00 +0200

    fix color component

--- components/technology/TechnologyItem.tsx
+++ components/technology/TechnologyItem.tsx
@@ -8,7 +8,7 @@ interface TechnologyItemProps {
 
 const TechnologyItem: React.FC<TechnologyItemProps> = ({ name, color, svg }) => {
   return (
-    <li className="flex items-center gap-3 rounded-md p-3 bg-secondary/5 bg-black border border-slate-800 max-w-full">
+    <li className="flex items-center gap-3 rounded-md p-3 bg-secondary/5 bg-neutral-900 border border-slate-800 max-w-full">
       <div className={`p-2 items-center justify-center rounded-lg bg-opacity-15`} style={{ backgroundColor: color }}>
         {svg}
       </div>
