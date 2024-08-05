3a56ba9d210650bd2c624f7f1747c6697aa9418a
Javor Emir <emir.j2002@gmail.com>
2024-08-05 20:43:02 +0200

    upd hero

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,34 +3,43 @@ import React from "react";
 const Navbar = () => {
   return (
     <header className=" mx-[10%] mt-2 shadow">
-      <div className="container  py-3">
+      <div className="hidden md:block md:container  md:py-3">
         <div className="flex items-center justify-between">
-            <div>
-                <a
-                className="font-bold text-2xl lg:text-4xl"
-                href="#"
-                >
-                EMIR 
-                </a>
-            </div>
-    
-            <nav className="space-x-2 text-lg ">
-                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#about">
-                About
-                </a>
-                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#portfolio">
-                Timeline
-                </a>
-                <a className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full" href="#services">
-                Tech Stack
-                </a>
-                <a className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full" href="#contact">
-                Contact me
-                </a>
-                {/* <button id="changeLanguage" className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full">
+          <div>
+            <a className="font-bold text-2xl lg:text-4xl" href="#">
+              EMIR
+            </a>
+          </div>
+
+          <nav className="space-x-2 text-lg ">
+            <a
+              className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full"
+              href="#about"
+            >
+              About
+            </a>
+            <a
+              className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full"
+              href="#portfolio"
+            >
+              Timeline
+            </a>
+            <a
+              className="hover:text-gray-300  bg-black border-2 border-gray-800 hover:bg-gray-900 py-2 px-4 rounded-full"
+              href="#services"
+            >
+              Tech Stack
+            </a>
+            <a
+              className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full"
+              href="#contact"
+            >
+              Contact me
+            </a>
+            {/* <button id="changeLanguage" className="text-black hover:text-gray-900  bg-white hover:bg-slate-200 py-2 px-4 rounded-full">
                 Change Language
                 </button> */}
-            </nav>
+          </nav>
         </div>
       </div>
     </header>
