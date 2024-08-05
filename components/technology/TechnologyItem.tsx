d6f5eeadf7b224d24d90a25de7321d7b178669a1
Javor Emir <emir.j2002@gmail.com>
2024-08-05 21:12:23 +0200

    svg glow

--- components/technology/TechnologyItem.tsx
+++ components/technology/TechnologyItem.tsx
@@ -8,8 +8,24 @@ interface TechnologyItemProps {
 
 const TechnologyItem: React.FC<TechnologyItemProps> = ({ name, color, svg }) => {
   return (
-    <li className="flex items-center gap-3 rounded-md p-3 bg-secondary/5 bg-neutral-900 border border-slate-800 max-w-full">
-      <div className={`p-2 items-center justify-center rounded-lg bg-opacity-15`} style={{ backgroundColor: color }}>
+    <li 
+      className="flex items-center gap-3 rounded-md p-3 bg-secondary/5 bg-neutral-900 border border-slate-800 max-w-full hover:bg-neutral-800"
+    >
+      <div 
+        className={`p-2 items-center justify-center rounded-lg bg-opacity-15 transition-transform transform`} 
+        style={{ 
+          backgroundColor: color, 
+          boxShadow: '0 0 10px rgba(255, 255, 255, 0)', /* Initial shadow */
+        }}
+        onMouseEnter={(e) => {
+          (e.currentTarget as HTMLElement).style.transform = 'scale(1.1)';
+          (e.currentTarget as HTMLElement).style.boxShadow = '0 0 10px 3px rgba(255, 255, 255, 0.5)'; /* Subtle white aura on hover */
+        }}
+        onMouseLeave={(e) => {
+          (e.currentTarget as HTMLElement).style.transform = 'scale(1)';
+          (e.currentTarget as HTMLElement).style.boxShadow = '0 0 10px rgba(255, 255, 255, 0)'; /* Remove aura on mouse leave */
+        }}
+      >
         {svg}
       </div>
       <span className="text-smaller md:text-small">{name}</span>
