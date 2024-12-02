54502f20e967d8f6eef504b925f4ea6b8ed15e38
Javor Emir <emir.j2002@gmail.com>
2024-12-02 11:03:54 +0100

    👌 IMPROVE: decrease time to display

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowPortfolio(true);
-    }, 3000); // Trigger after 3 seconds
+    }, 1000); 
 
     return () => clearTimeout(timer);
   }, []);
